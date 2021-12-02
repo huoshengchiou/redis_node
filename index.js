1da2dfb13eae4a7f4c57e28adafef633ae0f40cf
HSC <55527802+huoshengchiou@users.noreply.github.com>
2021-12-02 16:51:41 +0800

    [adjust]

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const redis = require("redis");
 const axios = require("axios");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const redisCache = false;
 
@@ -16,13 +17,14 @@ const DEFAULT_EXPIRATION = 3600;
 
 app.use(cors());
 
+app.use(bodyParser.json()); //for json req with  Content-type: applciation/json get  json from req.body
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.send("hello from node server");
 });
 
-app.get("/poke", async (req, res, next) => {
+app.get("/poke", (req, res, next) => {
   //在redis進行key的搜索，找到之前的資料就直接回送cache資料
   redisClient.get("pokemons", async (error, list) => {
     if (error) {
@@ -32,7 +34,7 @@ app.get("/poke", async (req, res, next) => {
       res.json(JSON.parse(list));
       return;
     }
-    //當redis 沒有cache資料時，才call api拉資料
+    //當redis 沒有cache資料時，call api
     const payload = await axios({
       url: "https://pokeapi.co/api/v2/pokemon",
       method: "get",
@@ -42,6 +44,29 @@ app.get("/poke", async (req, res, next) => {
       redisClient.setex("pokemons", DEFAULT_EXPIRATION, JSON.stringify(payload.data.results));
     }
     res.json(payload.data.results);
+  });
+});
+
+app.get("/evo", (req, res) => {
+  const { id } = req.query;
+  redisClient.get(`pokemon_${id}`, async (err, data) => {
+    if (err) {
+      console.error({ err });
+      return res.send("error");
+    }
+    if (data !== null) {
+      res.json(JSON.parse(data));
+      return;
+    }
+    const payload = await axios({
+      url: `https://pokeapi.co/api/v2/pokemon/${id}/`,
+      method: "get",
+    });
+
+    if (redisCache) {
+      redisClient.setex(`pokemon_${id}`, DEFAULT_EXPIRATION, JSON.stringify(payload.data));
+    }
+    res.json(payload.data);
   });
 });
 
