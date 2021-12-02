f6f484679d234c2c483f92e895d2cbbb6d82a6ee
HSC <55527802+huoshengchiou@users.noreply.github.com>
2021-12-02 15:16:13 +0800

    [modi]change api

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const Redis = require("redis");
+const redis = require("redis");
 const axios = require("axios");
 const cors = require("cors");
 
-// Create a Redis client for local development
-const redisClient = Redis.createClient();
+const redisCache = false;
+
+// Create a redis client for local development
+const redisClient = redis.createClient();
 // Or for production, pass in your production instance's URL
-// const redisClient = Redis.createClient({ url: "your-production-url"})
+// const redisClient = redis.createClient({ url: "your-production-url"})
 
 const app = express();
 
@@ -17,35 +19,32 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("hello world");
+  res.send("hello from node server");
 });
 
-app.get("/todo", async (req, res, next) => {
+app.get("/poke", async (req, res, next) => {
   //在redis進行key的搜索，找到之前的資料就直接回送cache資料
-  redisClient.get("todoList", async (error, todos) => {
-    if (todos) {
-      console.log({ error });
+  redisClient.get("pokemons", async (error, list) => {
+    if (error) {
+      console.error({ error });
     }
-    if (data !== null) {
-      res.json(JSON.stringify(todos));
+    if (list !== null) {
+      res.json(JSON.parse(list));
       return;
     }
+    //當redis 沒有cache資料時，才call api拉資料
+    const payload = await axios({
+      url: "https://pokeapi.co/api/v2/pokemon",
+      method: "get",
+    });
+    // console.log({ payload });
+    if (redisCache) {
+      redisClient.setex("pokemons", DEFAULT_EXPIRATION, JSON.stringify(payload.data.results));
+    }
+    res.json(payload.data.results);
   });
-
-  //當redis 沒有cache資料時，才call api拉資料
-  const payload = await axios({
-    url: "https://jsonplaceholder.typicode.com/todos",
-    method: "get",
-  });
-  console.log({ test: payload.data });
-  redisClient.setex(
-    "todoList",
-    DEFAULT_EXPIRATION,
-    JSON.stringify(payload.data)
-  );
-  res.json(payload.data);
 });
 
 app.listen(9876, () => {
-  console.log(" listening on 9876");
+  console.log(" node-server on 9876");
 });
